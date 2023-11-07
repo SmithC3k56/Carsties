d375416319406d2b742eed47149d58d77f44db3f
Smith <berusonc3k56@protonmail.com>
2023-11-07 12:15:09 +0700

    finished cli side

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -9,6 +9,7 @@ import Filters from './Filters';
 import { useParamsStore } from '@/hooks/useParamsStore';
 import { shallow } from 'zustand/shallow';
 import qs from 'query-string';
+import EmptyFilter from '../components/EmptyFilter';
 
 
 export default function Listings() {
@@ -16,35 +17,46 @@ export default function Listings() {
   const params = useParamsStore(state => ({
     pageNumber: state.pageNumber,
     pageSize: state.pageSize,
-    searchTerm: state.searchTerm
+    searchTerm: state.searchTerm,
+    orderBy: state.orderBy,
+    filterBy: state.filterBy
   }), shallow);
 
   const setParams = useParamsStore(state => state.setParams);
   const url = qs.stringifyUrl({ url: '', query: params });
 
-  function setPageNumber(pageNumber:number){
-    setParams({pageNumber});
+  function setPageNumber(pageNumber: number) {
+    setParams({ pageNumber });
   }
 
   useEffect(() => {
     getData(url).then(data => {
-      setData (data);
+      setData(data);
     })
   }, [url]);
 
   if (!data) return <h3>Loading...</h3>
+
+
   return (
     <>
-      <Filters  />
-      <div className='grid grid-cols-4 gap-6'>
+      <Filters />
+      {data.totalCount === 0 ? (
+        <EmptyFilter showReset />
 
-        {data.results.map(auction => (
-          <AuctionCard auction={auction} key={auction.id} />
-        ))}
-      </div>
-      <div className='flex justify-center mt-4'>
-        <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount} />
-      </div>
+      ) : (
+        <>
+          <div className='grid grid-cols-4 gap-6'>
+
+            {data.results.map(auction => (
+              <AuctionCard auction={auction} key={auction.id} />
+            ))}
+          </div>
+          <div className='flex justify-center mt-4'>
+            <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount} />
+          </div>
+        </>
+      )}
     </>
   )
 }
---
+++ frontend/web-app/app/components/Heading.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+type Props ={
+ title: string,
+ subtitle: string,
+ center?: boolean
+}
+
+export default function Heading({center,title,subtitle}:Props) {
+  return (
+    <div className={center ? 'text-center':'text-start'}>
+     <div className="text-2xl font-bold">
+      {title}
+     </div>
+     <div className="font-light text-neutral-500 mt-2">
+      {subtitle}
+     </div>
+    </div>
+  )
+}
