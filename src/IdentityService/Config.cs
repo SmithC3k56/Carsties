3d1160f251b7b23e3f2dcc1b9379f0213597e7c3
Smith <berusonc3k56@protonmail.com>
2023-11-14 10:30:10 +0100

    run docker

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -17,7 +17,7 @@ public static class Config
             new ApiScope("auctionApp","Aution app full access")
         };
 
-    public static IEnumerable<Client> Clients =>
+    public static IEnumerable<Client> Clients(IConfiguration config) =>
         new Client[]
         {
             new Client{
@@ -35,7 +35,7 @@ public static class Config
                 ClientSecrets ={new Secret("secret".Sha256())},
                 AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
                 RequirePkce = false,
-                RedirectUris ={"http://localhost:3000/api/auth/callback/id-server"},
+                RedirectUris ={config["ClientApp"]+"/api/auth/callback/id-server"},
                 AllowOfflineAccess = true,
                 AllowedScopes={"openid","profile","auctionApp"},
                 AccessTokenLifetime = 3600*24*30 ,// for 30 days
